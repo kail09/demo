43ae8831940f4fb743d572ab191d3dc77465213a
hong-gwon-il <hong-gwon-il@192.168.219.107>
2020-01-29 00:34:20 +0900

    테스트 웹페이지 개

---
+++ demo_20200102/src/main/java/com/demo/sample/jsp/JspController.java
@@ -0,0 +1,14 @@
+package com.demo.sample.jsp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+ 
+@Controller
+public class JspController {
+
+    @RequestMapping(value="/")      // localhost
+    public String root() {
+        return "index";          	// 실제 호출될 /WEB-INF/jsp/viewtest.jsp        
+    }
+}
