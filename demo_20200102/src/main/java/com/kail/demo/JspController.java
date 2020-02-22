8a97bccbacc9fdd5339043735d9ea71aee334224
hong-gwon-il <hong-gwon-il@172.30.1.43>
2020-02-23 06:51:04 +0900

    메인 화면 -> 로그인 화면으로 전환 및 로직 임시 처리

--- demo_20200102/src/main/java/com/kail/demo/JspController.java
+++ demo_20200102/src/main/java/com/kail/demo/JspController.java
@@ -6,15 +6,35 @@ import org.springframework.web.bind.annotation.ResponseBody;
  
 @Controller
 public class JspController {
-
-    @RequestMapping(value="/main")      // localhost
-    public String main() {
-        return "index";          	// 실제 호출될 /WEB-INF/view/index.jsp        
-    }
-    
-    @RequestMapping("/login") 
-    public @ResponseBody String login(){ 
-    	return "forward:/chk_login"; 
-    }
-    // 로그인화면 작업 (security 이용고민)
+	
+	boolean result = true;
+	
+	@RequestMapping(value="/")      // localhost
+	public String main() {
+		// 로그인 정보 여부 확인 
+		// 없을 경우 로그인 화면으로 리다이렉트 
+		if (result) {
+			return "redirect:/login_Admin";
+		} else {
+			return "redirect:/index_Admin";
+		}
+	}
+	
+	@RequestMapping("/login_Admin") 
+	public String login(){ 
+		if (result) {
+			return "/login_Admin";
+		} else {
+			return "redirect:/index_Admin";
+		}	}
+	
+	@RequestMapping("/index_Admin") 
+	public String index(){ 
+		if (result) {
+			return "redirect:/login_Admin";
+		} else {
+			return "/index_Admin";
+		}
+	}
+	// 로그인화면 작업 (security 이용고민)
 }
