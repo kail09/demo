507b35a2a53d07c65b22baa75cfa165fe64bdb48
hong-gwon-il <hong-gwon-il@192.168.219.100>
2020-02-27 22:06:10 +0900

    login / index 페이지 연동 및 로그인 값 점검

--- demo_20200102/src/main/java/com/kail/demo/common/JspController.java
+++ demo_20200102/src/main/java/com/kail/demo/common/JspController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.kail.demo.v1.member.model.MemberModel;
 import com.kail.demo.v1.member.service.MemberService;
 
+import io.micrometer.core.instrument.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j 
@@ -25,83 +26,82 @@ public class JspController {
 	@RequestMapping(value="/")      // localhost
 	public String main(HttpServletRequest req) {
 		
-		System.out.println("chk http data : " + req.toString());
 		
 		// 로그인 정보 여부 확인 
 		// 없을 경우 로그인 화면으로 리다이렉트 
 //		if (result) {
 			return "redirect:/login_Admin";
-/*		} else {
-			return "/index_Admin";
-		}*/
 	}
 	
 	@RequestMapping(value="/login_Admin") 
-	public String login(HttpServletRequest req, HttpSession session){ 
+	public String login(HttpServletRequest request){ 
 		
-		HttpSession Reqsession = req.getSession();
-		System.out.println("data chk : " + req.toString());
-		System.out.println("data chk : " + Reqsession.toString());
+		MemberModel memberModel = new MemberModel();
 
+		String resValue = null;
+		String resUrl = null;
 
-		if (result) {
-			System.out.println("chk login admin1");
-			return "/login_Admin";
-		} else {
-			System.out.println("chk login admin2");
-			return "/index_Admin";
-		}	
+		// 세션 값 리턴 및 값이 없을 경우 세션 생성 
+		HttpSession session = request.getSession(true);
+		
+		// session null 값 처리
+		try {
+			memberModel = (MemberModel) session.getAttribute("member");		
+			resValue = memberModel.getUserName();
+			
+			if (StringUtils.isNotBlank(resValue)) {
+				resUrl = "redirect:/index_Admin";
+			}
+		} catch (Exception e) {
+			resUrl = "/login_Admin";
+		}
+		return resUrl;
 	}
 	
-	@RequestMapping(value="/login_Admin/login", method=RequestMethod.POST) 
-	public String login1(HttpServletRequest req){ 
+	@RequestMapping(value="/login_Admin/login") 
+	public String login1(HttpServletRequest request){ 
+		
 		MemberModel model = new MemberModel();
-		HttpSession session = req.getSession();
-		log.debug("data chk : " + session.getAttribute("userId"));
-		System.out.println("chk data :  "+ session.getAttribute("userId"));
-		model.setUserId(req.getParameter("userId"));
-		model.setUserPw(req.getParameter("userPw"));
+		MemberModel vo = new MemberModel();
+		
+		HttpSession session = request.getSession();
+		String resUrl = null;
+		
+		model.setUserId(request.getParameter("userId"));
+		model.setUserPw(request.getParameter("userPw"));
 		
 		String userName = memberService.loginUserInfo(model);
-		MemberModel vo = new MemberModel();
-		if (userName == null ) {
-			session.setAttribute("member", null);
-			return "redirect:/login_Admin";
-		} else {
+		
+		if (StringUtils.isNotBlank(userName)) {
 			vo.setUserName(userName);
 			session.setAttribute("member", vo);
-			return "redirect:/index_Admin";
+			resUrl = "redirect:/index_Admin";
+		} else {
+			resUrl = "redirect:/login_Admin";
 		}
-		
-//		return "forward:/index_Admin";
-//		if (result) {
-//			System.out.println("chk login admin1");
-//			return "/login_Admin";
-//		} else {
-//			System.out.println("chk login admin2");
-//			return "/index_Admin";
-//		}	
+		return resUrl;
 	}
 	
 	@RequestMapping(value="/index_Admin") 
-	public String index(HttpServletRequest request, String res){ 
+	public String index(HttpServletRequest request){ 
+		MemberModel memberModel = new MemberModel();
+		String resValue = null;
+		String resUrl = null;
+		
 		// 세션 값 리턴 및 값이 없을 경우 세션 생성 
+		try {
+			HttpSession session = request.getSession(true);
+			memberModel = (MemberModel) session.getAttribute("member");		
+			resValue = memberModel.getUserName();
+		} catch (Exception e) {
+			log.error("Exception. Not Session ");
+		}
 		
-		HttpSession Reqsession = request.getSession();
-		System.out.println("data chk : " + request.toString());
-		System.out.println("data chk : " + Reqsession.toString());
-		MemberModel name = (MemberModel) Reqsession.getAttribute("member");		
-		
-		System.out.println("name : "+ name.getUserName());
-//		if (!"".equals(session.getAttribute("userId"))) {
-//			result = false;
-//		}
-		
-//		if (result) {
-//			return "redirect:/login_Admin";
-//		} else {
-			return "/index_Admin";
-//		}
+		if (StringUtils.isBlank(resValue)) {
+			resUrl = "redirect:/login_Admin";
+		} else {
+			resUrl = "/index_Admin";
+		}
+		return resUrl;
 	}
-	// 로그인화면 작업 (security 이용고민)
 }
